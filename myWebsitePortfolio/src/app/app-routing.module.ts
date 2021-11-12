0ca529d12642effc90bbd4590fc54fcd04061321
Robert Lillyblad <robert.lillyblad@gmail.com>
2021-11-11 21:43:26 -0600

    Added Welcome page.

--- myWebsitePortfolio/src/app/app-routing.module.ts
+++ myWebsitePortfolio/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent} from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 const routes: Routes = [
-{
-  path: '',
-  component: HomeComponent
 
-}
+  {path: '', component: WelcomeComponent},
+  {path: 'home-component', component: HomeComponent},
 ];
 
 @NgModule({
---
+++ myWebsitePortfolio/src/app/welcome/welcome.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+
+  goToPage(pageName:string): void
+  {
+    this.router.navigate(['$pageName']);
+  }
+  
+  ngOnInit(): void {
+  }
+
+}
